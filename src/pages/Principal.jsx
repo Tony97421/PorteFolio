ee2d3120b698129f3db4f2f64bfdb971d7f6901d
Techer Tony <techer307@gmail.com>
2024-01-01 12:15:16 +0100

    new modif

--- src/pages/Principal.jsx
+++ src/pages/Principal.jsx
@@ -3,59 +3,61 @@ import ButtonSwitch from "../components/ButtonSwitch";
 import { NavLink } from "react-router-dom";
 import NavContact from "../components/NavContact";
 
+
 const Principal = () => {
   return (
     <>
-    <NavContact />
+      <NavContact />
       <div className="containerPrincipal">
         <div className="containerNameEtTitre">
           <p className="name">Tony TECHER</p>
-          <p className="Porte">Porte</p>
-          <p className="Folio">Folio</p>
-          <div className="containerProjets">
-            <h3 className="pro">Pro</h3>
-            <h3 className="jets">jets :</h3>
-            <section className="projectCard">
-              <div className="projet1">
-                <h1>Sharring is carring</h1>
-                <a
-                  href="https://dreallers.github.io/sharing_is_caring/"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <img
-                    className="sharring"
-                    src="/images/sharring.png"
-                    alt="sharring is carring"
-                  />
-                </a>
-              </div>
-              <div className="projet2">
-                <h1>Wildclash</h1>
-                <a
-                  href="https://clash-heroes-eazaswc3f-techer-tonys-projects.vercel.app/"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <img
-                    className="wildClash"
-                    src="/images/wildClash.png"
-                    alt="wildClash"
-                  />
-                </a>
-              </div>
-              <div className="projet3">
-                <h1>WildTube</h1>
-                <NavLink to="/EnCours">
-                  <img
-                    className="wildTube"
-                    src="/images/wildTube.png"
-                    alt="wildTube"
-                  />
-                </NavLink>
-              </div>
-            </section>
+          <div className="titles">
+            <p className="porte">Porte</p>
+            <p className="folio">Folio</p>
           </div>
+        </div>
+        <div className="containerProjets">
+          <h3 className="pro">Projets :</h3>
+          <section className="projectCard">
+            <div className="projet">
+              <h1>Sharring is carring</h1>
+              <a
+                href="https://dreallers.github.io/sharing_is_caring/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img
+                  className="sharring"
+                  src="/images/sharring.png"
+                  alt="sharring is carring"
+                />
+              </a>
+            </div>
+            <div className="projet">
+              <h1>Wildclash</h1>
+              <a
+                href="https://clash-heroes-eazaswc3f-techer-tonys-projects.vercel.app/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img
+                  className="wildClash"
+                  src="/images/wildClash.png"
+                  alt="wildClash"
+                />
+              </a>
+            </div>
+            <div className="projet">
+              <h1>WildTube</h1>
+              <NavLink to="/EnCours">
+                <img
+                  className="wildTube"
+                  src="/images/wildTube.png"
+                  alt="wildTube"
+                />
+              </NavLink>
+            </div>
+          </section>
         </div>
       </div>
       <ButtonSwitch />
